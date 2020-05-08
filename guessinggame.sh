7d1ac45bc45358e66a24cf905558a0015e5c21f4
aarce <aart@spek-vdwekke.nl>
2020-05-08 08:34:42 +0200

    Implemented if-logic in guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,3 +1,15 @@
 number_of_files=$(ls | wc -l)
 echo $number_of_files
-
+echo "How many files have I got in my map..?"
+echo "type the number and press enter - please.."
+read answer
+echo "you typed: $answer."
+if [[ $answer -lt $number_of_files ]]
+then
+	echo "I'd say it's a bit more than that..."
+elif [[ $answer -gt $number_of_files ]]
+then
+	echo "I'd say it's a bit less than that..."
+else
+	echo "That is correct - congratulations!"
+fi
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # Welcome to the *guessinggame*-project
 make date and time comes here
-guessinggame.sh now has 3 lines of code!
+guessinggame.sh now has 15 lines of code!
